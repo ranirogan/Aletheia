f762e36ef7c667b1701629e40654ddd57c705db5
ranirogan <rrogan@smu.edu>
2021-03-07 14:45:01 -0600

    cleaned up code

--- Aletheia/popup.js
+++ Aletheia/popup.js
@@ -8,7 +8,8 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
    var test = document.getElementById("test");
    test.addEventListener('click', function(){
     var message = document.querySelector('#message');
-    message.innerHTML = "Injecting Script....";
+    // toggles text on
+    message.innerHTML = "Reading document";
     chrome.tabs.executeScript(null, {
       file: "getPageSource.js"
     }, function() {
@@ -16,23 +17,6 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
       if (chrome.runtime.lastError) {
         message.innerText = 'Cannot read from extensions. Please try again on a valid page. \n';
       }
-      });
     });
   });
-
-  // function onWindowLoad() {
-  
-  //   var message = document.querySelector('#message');
-  
-  //   chrome.tabs.executeScript(null, {
-  //     file: "getPageSource.js"
-  //   }, function() {
-  //     // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-  //     if (chrome.runtime.lastError) {
-  //       message.innerText = 'Cannot read from extensions. Please try again on a valid page. \n';
-  //     }
-  //   });
-  
-  // }
-  
-  //window.onload = onWindowLoad;
+});
