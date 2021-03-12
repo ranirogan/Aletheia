b2ad57b484d80b8d68c4843c27d8ebd65dccc8b2
ranirogan <rrogan@smu.edu>
2021-03-12 12:17:04 -0600

    will need to fix execute script statement? unsure cause, commented out

--- Aletheia/popup.js
+++ Aletheia/popup.js
@@ -9,17 +9,17 @@ document.addEventListener('DOMContentLoaded', function(){
   read.addEventListener('click', function(){
     var button = document.getElementById('read')
     button.disabled = true;
-    var message = document.querySelector('#message');
-    // toggles text on
-    message.innerHTML = "Reading document";
-    chrome.tabs.executeScript(null, {
-      file: "getPageSource.js"
-    }, function() {
-      // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-      if (chrome.runtime.lastError) {
-        message.innerText = 'Cannot read from extensions. Please try again on a valid page. \n';
-      }
-    });
+    // var message = document.querySelector('#message');
+    // // toggles text on
+    // message.innerHTML = "Reading document";
+    // chrome.tabs.executeScript(null, {
+    //   file: "getPageSource.js"
+    // }, function() {
+    //   // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    //   if (chrome.runtime.lastError) {
+    //     message.innerText = 'Cannot read from extensions. Please try again on a valid page. \n';
+    //   }
+    // });
     var digest = document.querySelector("#digest");
     digest.classList.add("active");
     var welcome = document.querySelector("#welcome");
