1228c29b8004437615e531603e6cf85544d2604d
ranirogan <rrogan@smu.edu>
2021-03-13 15:25:32 -0600

    links work now

--- Aletheia/popup.js
+++ Aletheia/popup.js
@@ -5,6 +5,16 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
 });
   
 document.addEventListener('DOMContentLoaded', function(){
+  var links = document.getElementsByTagName("a");
+  for(var i = 0; i < links.length; i++){
+    (function (){
+      var link = links[i];
+      var location = link.href;
+      link.onclick = function(){
+        chrome.tabs.create({active: true, url: location});
+      }
+    })();
+  }
   var read = document.getElementById("read");
   read.addEventListener('click', function(){
     var button = document.getElementById('read')
