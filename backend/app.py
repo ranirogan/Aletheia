dd4c76c358d09e500d2688d58a540ef89b61f893
Troy McNitt <tmcnitt@smu.edu>
2021-04-01 16:04:32 -0500

    Get similar articles

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,8 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import spacy
+import feedparser
+import urllib.parse
 
 # python -m spacy download en_core_web_sm
 nlp = spacy.load("en_core_web_md")
@@ -31,7 +33,25 @@ def news():
     score = sid.polarity_scores(text)
 
     parsed = nlp(text)
-    ents = [{'text': e.text, 'label': e.label_} for e in parsed.ents]
+    ents = [{'text': e.text, 'label': e.label_,
+             'count': text.count(e.text)} for e in parsed.ents]
+
+    url = "https://news.google.com/rss/search?q={}".format(
+        urllib.parse.quote_plus(article.title))
+    feed = feedparser.parse(url)
+    entries = feed.entries[1:11]
+    entries = [{'title': entry.title, 'url': entry.links[0].href}
+               for entry in entries]
+
+    for entry in entries:
+        try:
+            compare = Article(entry['url'])
+            compare.download()
+            compare.parse()
+            sid = SentimentIntensityAnalyzer()
+            entry['sediment'] = sid.polarity_scores(compare.text)
+        except:
+            print('Error getting article')
 
     return jsonify(
         authors=article.authors,
@@ -40,9 +60,10 @@ def news():
         keywords=article.keywords,
         summary=article.summary,
         sentiment=score,
-        entities=ents
+        entities=ents,
+        similar=entries
     )
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
