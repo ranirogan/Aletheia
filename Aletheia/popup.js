e31f7ec01a481eb81aa7de1574ee3df57cfa00a3
ranirogan <rrogan@smu.edu>
2021-04-05 16:20:32 -0500

    uses current url of tab

--- Aletheia/popup.js
+++ Aletheia/popup.js
@@ -25,15 +25,19 @@ document.addEventListener('DOMContentLoaded', function(){
     welcome.classList.remove("active");
     var message = document.querySelector('#message');
     message.classList.add("active");
-    fetch('http://ec2-18-144-60-190.us-west-1.compute.amazonaws.com:8080/', {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+      let url = tabs[0].url;
+      fetch('http://ec2-18-144-60-190.us-west-1.compute.amazonaws.com:8080/', {
       method: 'POST',
-      body: 'https://apnews.com/article/derek-chauvin-trial-live-updates-c3e3fe08773cd2f012654e782e326f6e'
-    })
-    .then(function(response) {
-      console.log(response);
-      return response.text().then(function(text) {
-        message.innerHTML = text;
+      body: url
+      })
+      .then(function(response) {
+        console.log(response);
+        return response.text().then(function(text) {
+          message.innerHTML = text;
+        });
       });
     });
+    
   });
 });
