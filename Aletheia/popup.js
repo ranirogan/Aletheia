a93944f921d9417359b5a587ab0a30d940ee384e
ranirogan <rrogan@smu.edu>
2021-04-08 15:33:48 -0500

    some bootstrap styling

--- Aletheia/popup.js
+++ Aletheia/popup.js
@@ -1,9 +1,3 @@
-chrome.runtime.onMessage.addListener(function(request, sender) {
-  if (request.action == "getSource") {
-    message.innerText = request.source;
-  }
-});
-  
 document.addEventListener('DOMContentLoaded', function(){
   var read = document.getElementById("read");
   read.addEventListener('click', function(){
@@ -20,18 +14,17 @@ document.addEventListener('DOMContentLoaded', function(){
       fetch('http://ec2-18-144-60-190.us-west-1.compute.amazonaws.com:8080/', {
       method: 'POST',
       body: url
-      })
+    })
       .then(function(response) {
         return response.json().then(json => getVals(json))
       });
     });
-    
   });
 });
 
 function getVals(json){
   var title = document.getElementById("title");
-  title.innerHTML = json.title;
+  title.innerText = json.title;
   var author = document.getElementById("author");
   author.innerHTML = json.authors;
   var sum = document.getElementById("summary");
@@ -43,24 +36,29 @@ function getVals(json){
   getEntities(entities);
   var similar = json.similar;
   getSimilar(similar);
-  activateLinks();
+  var keyTerms = document.getElementById("keyTerms");
+  activateLinks(keyTerms);
+  var articles = document.getElementById("articles");
+  activateLinks(articles);
+  var message = document.querySelector('#message');
+  message.classList.remove("active");
 }
 
 function getEntities(entities){
   var keyTerms = document.getElementById("keyTerms");
   for(var i = 0; i < entities.length; i++){
     var ent = entities[i];
-    var li = document.createElement("li");
     var a = document.createElement("a");
     a.innerHTML = ent.text;
     a.setAttribute("href", ent.wiki);
-    li.appendChild(a);
-    keyTerms.appendChild(li);
+    a.classList.add("list-group-item");
+    a.classList.add("list-group-item-action");
+    keyTerms.appendChild(a);
   }
 }
 
-function activateLinks(){
-  var links = document.getElementsByTagName("a");
+function activateLinks(place){
+  var links = place.getElementsByTagName("a");
   for(var i = 0; i < links.length; i++){
     (function (){
       var link = links[i];
@@ -74,12 +72,44 @@ function activateLinks(){
 
 function getSentiment(sent, sentiment){
   var fragment = document.createDocumentFragment();
-  var pos = document.createElement("p");
-  var neg = document.createElement("p");
-  var neu = document.createElement("p");
-  pos.innerHTML = sentiment.pos;
-  neg.innerHTML = sentiment.neg;
-  neu.innerHTML = sentiment.neu;
+  var pos = document.createElement("div");
+  var neg = document.createElement("div");
+  var neu = document.createElement("div");
+  var p = sentiment.pos * 100;
+  var ng = sentiment.neg * 100;
+  var nu = sentiment.neu * 100;
+
+  pos.classList.add("progress-bar");
+  pos.classList.add("bg-success");
+  pos.setAttribute("role", "progressbar");
+  pos.setAttribute("aria-valuemin", "0");
+  pos.setAttribute("aria-valuemax", "100");
+
+  neg.classList.add("progress-bar");
+  neg.classList.add("bg-danger");
+  neg.setAttribute("role", "progressbar");
+  neg.setAttribute("aria-valuemin", "0");
+  neg.setAttribute("aria-valuemax", "100");
+
+  neu.classList.add("progress-bar");
+  neu.classList.add("bg-secondary");
+  neu.setAttribute("role", "progressbar");
+  neu.setAttribute("aria-valuemin", "0");
+  neu.setAttribute("aria-valuemax", "100");
+
+  pos.setAttribute("aria-valuenow", p);
+  pos.setAttribute("style", `width: ${p}%`);
+
+  neg.setAttribute("aria-valuenow", ng);
+  neg.setAttribute("style", `width: ${ng}%`);
+
+  neu.setAttribute("aria-valuenow", nu);
+  neu.setAttribute("style", `width: ${nu}%`);
+
+  neg.innerText = Math.floor(ng) + "%";
+  neu.innerText = Math.floor(nu) + "%";
+  pos.innerText = Math.floor(p) + "%";
+
   fragment.appendChild(neg);
   fragment.appendChild(neu);
   fragment.appendChild(pos);
@@ -89,20 +119,19 @@ function getSentiment(sent, sentiment){
 function getSimilar(similar){
   var articles = document.getElementById("articles");
   for(var i = 0; i < similar.length; i++){
-    var li = document.createElement("li");
-    var p = document.createElement("p");
+    var div = document.createElement("div");
     var art = similar[i];
-    p.innerHTML = art.title;
-    li.appendChild(p);
     var a = document.createElement("a");
-    a.innerText = "From " + art.url.substring(0,100);
+    a.innerText = art.title;
     a.setAttribute("href", art.url);
-    li.appendChild(a);
+    div.appendChild(a);
     if(art.sediment){
-      var sed = document.createElement("p");
-      getSentiment(sed, art.sediment)
-      li.appendChild(sed);
+      var sed = document.createElement("div");
+      sed.classList.add("progress");
+      getSentiment(sed, art.sediment);
+      div.appendChild(sed);
     }
-    articles.appendChild(li);
+    div.classList.add("list-group-item");
+    articles.appendChild(div);
   }
 }
